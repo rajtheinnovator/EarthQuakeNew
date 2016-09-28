b15e9c4c6fc5f85d6a7c4a753bfd6f12d61fcd77
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-09-28 22:12:53 +0530

    async task for fetching json from usgs website added successfully

--- app/src/main/java/com/example/android/earthquakenew/QueryUtils.java
+++ app/src/main/java/com/example/android/earthquakenew/QueryUtils.java
@@ -134,7 +134,7 @@ public final class QueryUtils {
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
 
-            // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
+            // TODO: Parse the response given by the jsonResponse string and
             // build up a list of Earthquake objects with the corresponding data.
             JSONObject root = new JSONObject(jsonResponse);
             JSONArray features = root.getJSONArray("features");
