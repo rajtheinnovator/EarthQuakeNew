78d573755fc68705d3fbbc131b1df5f5cf534fbd
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-09-29 23:41:48 +0530

    code refactored by optimizing imports and unused code

--- app/src/main/java/com/example/android/earthquakenew/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/earthquakenew/EarthquakeActivity.java
@@ -1,12 +1,14 @@
 package com.example.android.earthquakenew;
 
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
+import android.content.Loader;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,17 +18,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.app.LoaderManager;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.Loader;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<ArrayList<Earthquake>> {
     /**
@@ -48,8 +41,6 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query";
 
-    //private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,9 +53,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             /*
             fetch data
             Get a reference to the LoaderManager, in order to interact with loaders.
-            */
-            /*
-        Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            And, Initialize the loader. Pass in the int ID constant defined above and pass in null for
         the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
         because this activity implements the LoaderCallbacks interface).
         */
@@ -82,6 +71,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             loadingIndicator.setVisibility(View.GONE);
         }
     }
+
     //Show the menu in ActionBar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
