a85912b547ad55b1173a70d3ceee8ef2e536c02d
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-09-29 23:22:27 +0530

    added SharedPreferences functionality

--- app/src/main/java/com/example/android/earthquakenew/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/earthquakenew/EarthquakeActivity.java
@@ -2,13 +2,17 @@ package com.example.android.earthquakenew;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -42,7 +46,9 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
-    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query";
+
+    //private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,9 +59,11 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            // fetch data
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-              /*
+            /*
+            fetch data
+            Get a reference to the LoaderManager, in order to interact with loaders.
+            */
+            /*
         Initialize the loader. Pass in the int ID constant defined above and pass in null for
         the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
         because this activity implements the LoaderCallbacks interface).
@@ -73,16 +81,45 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             View loadingIndicator = findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
         }
+    }
+    //Show the menu in ActionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-        //Log.v(LOG_TAG, "You have got a " + loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this));
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public Loader<ArrayList<Earthquake>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
-        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -90,18 +127,17 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         // Hide loading indicator because the data has been loaded
         View loadingIndicator = findViewById(R.id.loading_indicator);
         loadingIndicator.setVisibility(View.GONE);
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        // Set empty state text to display "No earthquakes found."
-        mEmptyStateTextView.setText(R.string.no_earthquakes);
 
         // Clear the adapter of previous earthquake data
         // earthquakeAdapter.clear();
         // If there is no result, do nothing.
         Log.v(LOG_TAG, "You have got a onLoadFinished");
         if (earthquakes == null) {
+            mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+            // Set empty state text to display "No earthquakes found."
+            mEmptyStateTextView.setText(R.string.no_earthquakes);
             return;
         }
-
 
         // Create a new {@link ArrayAdapter} of earthquakes
         earthquakeAdapter = new EarthquakeAdapter(EarthquakeActivity.this, earthquakes);
